77ee2468b620bcb0a54bcf22b7009e41870ab479
Brandon <bsm2112@yahoo.com>
2022-07-21 16:27:20 -0500

    Initial commit

---
+++ src/main/java/dev/bdon/interfaces/SinglyLinkedList.java
@@ -0,0 +1,98 @@
+package dev.bdon.interfaces;
+
+import java.util.Iterator;
+
+public interface SinglyLinkedList<T extends Comparable<T>> extends Iterable<T> {
+
+    interface Node<T> {
+        T getData();
+        Node<T> getNext();
+    }
+
+    /**
+     * Adds an item to front of the list where list[0] is considered the front
+     */
+    void pushFront(T data);
+
+    /**
+     * Adds an item to back of the list where list[size() - 1] is considered the back
+     */
+    void pushBack(T data);
+
+    /**
+     * Inserts an item to the specified position in the list.
+     *
+     * @throws IndexOutOfBoundsException if pos is < 0 OR pos > size() + 1
+     */
+    void insert(int pos, T data);
+
+    /**
+     * Returns the node of the front of the list
+     */
+    Node<T> head();
+
+    /**
+     * Returns the number of items in the list
+     */
+    int size();
+
+    /**
+     * Returns true if this list has zero items, false if otherwise.
+     *
+     * Note - if list.size() returns 0, then list.isEmpty() should also be returning true
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns true if this list contains the given item, false if otherwise
+     */
+    boolean contains(T o);
+
+    /**
+     * Returns an iterator for the items in this list
+     */
+    Iterator<T> iterator();
+
+    /**
+     * Removes the first-occurrence of a given item from the list.
+     * Returns true if the item existed and was removed, false if otherwise
+     */
+    boolean remove(T o);
+
+    /**
+     * Deletes all items from the list
+     */
+    void clear();
+
+    /**
+     * Returns the maximum item in the list as defined by its compareTo() method
+     */
+    T max();
+
+    /**
+     * Returns the minimum item in the list as defined by its compareTo() method
+     */
+    T min();
+
+    /**
+     * Walks the list to return a fixed-sized array of all the items in the list.
+     */
+    default T[] toArray() {
+        int count = 0;
+        Node<T> current = head();
+        while (current != null) {
+            ++count;
+            current = current.getNext();
+        }
+        T[] arr = (T[]) new Object[count];
+
+        count = 0;
+        current = head();
+        while (current != null) {
+            arr[count++] = current.getData();
+            current = current.getNext();
+        }
+
+        return arr;
+    }
+}
---
+++ src/test/java/dev/bdon/SinglyLinkedListTest.java
@@ -0,0 +1,184 @@
+package dev.bdon;
+
+import dev.bdon.interfaces.SinglyLinkedList;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("ConstantConditions")
+class SinglyLinkedListTest {
+
+    private <T extends Comparable<T>> SinglyLinkedList<T> createList() {
+        return null;
+    }
+
+    @Test
+    void should_insert_items_to_the_back_of_list() {
+        SinglyLinkedList<Integer> list = createList();
+        assert list.isEmpty();
+        assert list.size() == 0;
+        assert list.head() == null;
+
+        list.pushBack(1);
+        assert list.head() != null;
+        assert list.size() == 1;
+        assert !list.isEmpty();
+
+        list.pushBack(2);
+        list.pushBack(3);
+        list.pushBack(4);
+        list.pushBack(5);
+        assert list.size() == 5;
+
+        SinglyLinkedList.Node<Integer> current = list.head();
+        assert current.getData() == 1;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 2;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 3;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 4;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 5;
+        assert current.getNext() == null;
+    }
+
+    @Test
+    void should_insert_items_to_the_front_of_list() {
+        SinglyLinkedList<Integer> list = createList();
+        assert list.isEmpty();
+        assert list.size() == 0;
+        assert list.head() == null;
+
+        list.pushFront(1);
+        list.pushFront(2);
+        list.pushFront(3);
+        list.pushFront(4);
+        list.pushFront(5);
+        assert list.head() != null;
+        assert list.size() == 5;
+        assert !list.isEmpty();
+
+        SinglyLinkedList.Node<Integer> current = list.head();
+        assert current.getData() == 5;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 4;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 3;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 2;
+        assert current.getNext() != null;
+
+        current = current.getNext();
+        assert current.getData() == 1;
+        assert current.getNext() == null;
+    }
+
+    @Test
+    void should_test_if_list_contains_specified_items() {
+        SinglyLinkedList<String> list = createList();
+
+        assert !list.contains("Apple");
+        assert !list.contains("Daikon");
+
+        list.pushBack("Apple");
+        list.pushBack("Banana");
+        list.pushBack("Cherry");
+
+        assert list.contains("Apple");
+        assert !list.contains("Daikon");
+    }
+
+    @Test
+    void should_insert_items_into_arbitrary_positions_of_list() {
+        SinglyLinkedList<String> list = createList();
+
+        list.insert(0, "Banana");
+        assert Arrays.deepEquals(list.toArray(), arrayOf("Banana"));
+
+        list.insert(1, "Egg");
+        assert Arrays.deepEquals(list.toArray(), arrayOf("Banana", "Egg"));
+
+        list.insert(0, "Apple");
+        assert Arrays.deepEquals(list.toArray(), arrayOf("Apple", "Banana", "Egg"));
+
+        list.insert(2, "Daikon");
+        assert Arrays.deepEquals(list.toArray(), arrayOf("Apple", "Banana", "Daikon", "Egg"));
+
+        list.insert(2, "Cherry");
+        assert Arrays.deepEquals(list.toArray(), arrayOf("Apple", "Banana", "Cherry", "Daikon", "Egg"));
+
+        assert !list.isEmpty();
+        assert list.size() == 5;
+    }
+
+    @Test
+    void should_clear_list_after_adding_items() {
+        SinglyLinkedList<String> list = createList();
+
+        list.pushBack("1");
+        list.pushBack("2");
+        list.pushBack("3");
+        list.clear();
+
+        assert list.isEmpty();
+        assert list.size() == 0;
+        assert list.head() == null;
+    }
+
+    @Test
+    void should_remove_items_from_list_one_by_one() {
+        SinglyLinkedList<String> list = createList();
+
+        list.pushBack("1");
+        list.pushBack("2");
+        list.pushBack("3");
+        list.pushBack("4");
+        list.pushBack("5");
+
+        list.remove("1");
+        list.remove("4");
+        list.remove("5");
+        list.remove("3");
+        list.remove("2");
+
+        assert list.isEmpty();
+        assert list.size() == 0;
+        assert list.head() == null;
+    }
+
+    @Test
+    void should_return_the_max_and_min_items_in_the_list() {
+        SinglyLinkedList<Integer> list = createList();
+
+        list.pushFront(12);
+        list.pushFront(0);
+        list.pushFront(140);
+        list.pushFront(-333);
+        list.pushFront(38);
+
+        assert list.max() == 140;
+        assert list.min() == -333;
+    }
+
+    @SafeVarargs
+    private <T> T[] arrayOf(T... items) {
+        return items;
+    }
+}
